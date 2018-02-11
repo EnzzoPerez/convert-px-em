d450a0267c0aeb67f6f01b565fe0db491a94a5fe
Enzzo <enzzorp@gmail.com>
2018-02-11 12:11:29 -0300

    added convert function and handles(?

--- src/components/inputList.js
+++ src/components/inputList.js
@@ -10,34 +10,61 @@ class InputList extends Component {
 
         this.handleInput=this.handleInput.bind(this)
         this.handleSelect=this.handleSelect.bind(this)
+        this.convertidor=this.convertidor.bind(this)
     }
 
     handleInput(event){
         let target = event.target
-        console.log("IN=PUT", target.value);
         let from = {...this.state.from}
+        let to = {...this.state.to}
+
         from.input = target.value
+        to.input = this.convertidor(from.input, from.select, this.state.to.select)
+        
         this.setState({
-            from: from
+                from: from,
+                to: to
         })
+        
     }
 
     handleSelect(event){
         let target = event.target
+        let from = {...this.state.from}
+        let to = {...this.state.to}
 
         if (target.id === "from") {
-            let from = {...this.state.from}
             from.select = target.value
+            to.input = this.convertidor(from.input, from.select, this.state.to.select)
+
             this.setState({
-                from: from
+                from: from,
+                to: to
             })
         } else {
-            let to = {...this.state.to}
             to.select = target.value
+            to.input = this.convertidor(from.input, from.select, to.select)
+
             this.setState({
+                from: from,
                 to: to
             })
         }
+    }
+
+    convertidor(inpFrom, selFrom, selTo){
+        let valorConvertido
+        
+        if (selFrom === 'px'){
+            if (selTo === 'em' || selTo === 'rem') 
+                valorConvertido = parseFloat(inpFrom) * 0.063
+        }
+        else if(selFrom === 'em' || selFrom === 'rem'){
+            if (selTo === 'px') {
+                valorConvertido = parseFloat(inpFrom) * 16
+            } 
+        }
+        return valorConvertido
     }
 
 
@@ -55,7 +82,7 @@ class InputList extends Component {
                     {opciones}
                 </select>
 
-                <input type="number" disabled="true" value={this.state.to.input}/>
+                <input type="number" disabled="true" value={this.state.to.input || '0'}/>
                 <select id="to" value={this.state.to.select} onChange={this.handleSelect}>
                     {opciones}
                 </select>
