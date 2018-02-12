95dcca8d83748c9fd2a00535147065fc20e2623c
Enzzo <enzzorp@gmail.com>
2018-02-12 12:21:52 -0300

    update app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import InputList from "./components/inputList";
+import Header from "./components/header";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="contenedor">
+        <Header />
         <InputList />
       </div>
     );
